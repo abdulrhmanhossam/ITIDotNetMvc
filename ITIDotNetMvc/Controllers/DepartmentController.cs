f665f408500250d4190133e4242b4b564cd76e0d
abdulrahman <abdulrahmn398@outlook.com>
2025-02-17 20:33:16 +0200

    Add DepartmentController and update Index view
    Introduce DepartmentController to retrieve departments and employees using EF Core. Update Index.cshtml to display department details in a table.

---
+++ ITIDotNetMvc/Controllers/DepartmentController.cs
@@ -0,0 +1,18 @@
+﻿using ITIDotNetMvc.Models.Data;
+using ITIDotNetMvc.Models.Entites;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITIDotNetMvc.Controllers;
+
+public class DepartmentController : Controller
+{
+    AppDbContext dbContext = new AppDbContext(); 
+    public IActionResult Index()
+    {
+        List<Department> departmentList = dbContext
+            .Departments.Include(e => e.Employees).ToList();
+
+        return View("Index", departmentList); // Model ==> List<Department>
+    }
+}
