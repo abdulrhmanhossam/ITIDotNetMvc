e82e8c2930f1060bdf4cd69a9c24ec80dd11a926
abdulrahman <abdulrahmn398@outlook.com>
2025-02-20 12:49:47 +0200

    Implement EmployeeDepartmentViewModel for Details view
    Refactor the EmployeeController to use a new strongly-typed
    view model, `EmployeeDepartmentViewModel`, in the `Details`
    action, replacing the previous `ViewData` approach. Update
    the `Details.cshtml` view to utilize the new model, enhancing
    data management and type safety. Also, include the new
    namespace in `_ViewImports.cshtml` and create the
    `EmployeeDepartmentViewModel.cs` file to define the view
    model properties.

--- ITIDotNetMvc/Controllers/EmployeeController.cs
+++ ITIDotNetMvc/Controllers/EmployeeController.cs
@@ -1,5 +1,6 @@
 ﻿using ITIDotNetMvc.Models.Data;
 using ITIDotNetMvc.Models.Entites;
+using ITIDotNetMvc.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,7 +8,7 @@ namespace ITIDotNetMvc.Controllers;
 
 public class EmployeeController : Controller
 {
-    AppDbContext dbContext = new AppDbContext(); 
+    AppDbContext dbContext = new AppDbContext();
     public IActionResult Index()
     {
         var employeeList = dbContext.Employees
@@ -16,27 +17,59 @@ public class EmployeeController : Controller
         return View("Index", employeeList);
     }
 
+    #region ViewData and ViewBag
+    //public IActionResult Details(int id)
+    //{
+    //    // 3 info not related to Model
+    //    string msg = "Hello From Action";
+    //    int temp = 50;
+    //    List<string> branches = new List<string>
+    //    {
+    //        "Assuit",
+    //        "Alex",
+    //        "Cairo"
+    //    };
+
+    //    // using viewdata to send this info to view 
+    //    ViewData["Msg"] = msg;
+    //    ViewData["Temp"] = temp;
+    //    ViewData["Brch"] = branches;
+    //    // ViewData.Model = employeeModel; we can send model here
+
+    //    Employee employeeModel = dbContext.Employees
+    //        .FirstOrDefault(e => e.Id == id)!;
+
+    //    return View("Details", employeeModel);
+    //} 
+    #endregion
+
+
+    // using view model
     public IActionResult Details(int id)
     {
-        // 3 info not related to Model
-        string msg = "Hello From Action";
-        int temp = 50;
-        List<string> branches = new List<string>
+        var employee = dbContext.Employees
+            .Include(d => d.Department)
+            .FirstOrDefault(e => e.Id == id);
+
+        List<string> Branches = new List<string>
         {
             "Assuit",
             "Alex",
             "Cairo"
         };
+        // declare view model and map prop
+        var employeeDepartmentViewModel = new EmployeeDepartmentViewModel
+        {
+            EmployeeName = employee!.Name,
+            DepartmentName = employee.Department.Name,
+            Branches = Branches,
+            Color = "Red",
+            Msg = "Hello From View Model",
+            Temp = 12
+        };
 
-        // using viewdata to send this info to view 
-        ViewData["Msg"] = msg;
-        ViewData["Temp"] = temp;
-        ViewData["Brch"] = branches;
-        // ViewData.Model = employeeModel; we can send model here
-
-        Employee employeeModel = dbContext.Employees
-            .FirstOrDefault(e => e.Id == id)!;
-
-        return View("Details", employeeModel);
+        return View(employeeDepartmentViewModel);
     }
+
+
 }
---
+++ ITIDotNetMvc/ViewModel/EmployeeDepartmentViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ITIDotNetMvc.ViewModel;
+
+public class EmployeeDepartmentViewModel
+{
+    public string EmployeeName { get; set; }
+    public string DepartmentName { get; set; }
+    public List<string> Branches { get; set; }
+    public int Temp { get; set; }
+    public string Msg { get; set; }
+    public string Color { get; set; }
+}
